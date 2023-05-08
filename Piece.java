33607d3a0dab5b226a6b027b00145fc2069a8254
bdmitruk <01178272@pw.edu.pl>
2023-05-08 19:03:16 +0200

    Created first version of classes, fields and methods featured in the documentation of the previous stage.

---
+++ Piece.java
@@ -0,0 +1,6 @@
+public class Piece {
+    boolean isDame // true - jest damką, false - jest zwykłym pionkiem
+    boolean color // true - biały, false - czarny
+    Field field // referencja do pola na krórym stoi pionek
+
+}
