84c5956cad903c18adaa06072dcd638bf93405b1
Emilia Jasińska <jasinska0000@gmail.com>
2023-05-09 20:26:51 +0200

    pierwsze podejscie do serwera

--- src/Client.java
+++ src/Client.java
@@ -1,23 +1,103 @@
+import java.io.*;
+import java.net.*;
 public class Client {
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+    private Game game;
 
-    Socket Connect(IP){
-
+    public Client(Game game) {
+        this.game = game;
+    }
+    Socket Connect(String IP, int port){
+        try {
+            socket = new Socket(IP, port);
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+            startListening();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return socket;
     }
 
-    void SendMove(Game){
-
+    private void startListening() {
+        Thread listeningThread = new Thread(() -> {
+            while (!socket.isClosed()) {
+                try {
+                    String message = in.readUTF();
+                    processServerMessage(message);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        listeningThread.start();
     }
-    void ChangeBoard(Game){
 
+    private void processServerMessage(String message) {
+        String[] parts = message.split(" ");
+        switch (parts[0]) {
+            case "Change board":
+                game.updateBoard(parts[1]);
+                break;
+            case "Move":
+                Field start = game.getFieldById(Integer.parseInt(parts[1]));
+                Field end = game.getFieldById(Integer.parseInt(parts[2]));
+                game.move(start, end);
+                game.endTurn();
+                break;
+            case "Close game":
+                closeGame();
+                break;
+            default:
+                System.err.println("Unknown message received: " + message);
+                break;
+            if (message.startsWith("Error:")) {
+                String errorMessage = message.substring(6);
+                handleError(errorMessage);
+            } else if (message.startsWith("Score:")) {
+                int score = Integer.parseInt(message.substring(6));
+                handleScoreUpdate(score);
+            }
+        }
     }
-    void CloseGame(Boolean){
 
+    void SendMove(Field start, Field end) {
+        try {
+            out.writeUTF("Move " + start.getId() + " " + end.getId());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    void ChangeBoard(String newBoard){
+        try {
+            out.writeUTF("Change board " + newBoard);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    void CloseGame(){
+        try {
+            out.writeUTF("Close game");
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
+    void sendError(String errorMessage) {
+        try {
+            out.writeUTF("Error:" + errorMessage);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-
-
-
-
+    void handleError(String errorMessage) {
+        // Obsługa błędu, np. wyświetlenie komunikatu o błędzie użytkownikowi
+        System.err.println("Błąd: " + errorMessage);
+        // Możesz również wyświetlić komunikat o błędzie w interfejsie użytkownika
+    }
 
 }
--- src/FileManager.java
+++ src/FileManager.java
@@ -1,16 +1,50 @@
+import java.io.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
 public class FileManager {
+    private final ObjectMapper objectMapper;
+
+    public FileManager() {
+        objectMapper = new ObjectMapper();
+    }
     void saveGameState(Game, path){
         // Funkcja która zapisze w pliku zserializowaną instancję gry do wskazanego pliku.
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(path))) {
+            oos.writeObject(game);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     Game loadGameState(path ){
         // Funkcja która zwróci instancję gry na podstawie zserializowanego stanu we wskazanym pliku.
+        Game game = null;
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(path))) {
+            game = (Game) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return game;
     }
 
-    void saveOptions( UserInterface ){
+    void saveOptions(UserInterface userInterface, String path ){
         // Funkcja która pobierze dane ze wskazanej klasy i zapisze je w Pliku JSON.
+        try {
+            objectMapper.writeValue(new File(path), userInterface);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     void loadOptions( UserInterface ){
         // Funkcja która ustawi zmienne we wskazanej klasie na podstawie pliku JSON
+        UserInterface loadedUserInterface = null;
+        try {
+            loadedUserInterface = objectMapper.readValue(new File(path), UserInterface.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        // Aktualizacja opcji interfejsu użytkownika
+        userInterface.setOptions(loadedUserInterface.getOptions());
         }
 }
