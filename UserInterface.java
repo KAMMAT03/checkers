07cef9e5deff71ee7009a74852d92880ccff5ff5
Emilia Jasińska <jasinska0000@gmail.com>
2023-05-14 17:57:45 +0200

    interface

---
+++ UserInterface.java
@@ -0,0 +1,54 @@
+import java.io.Serializable;
+
+public class UserInterface implements Serializable {
+    private int boardSize;
+    private String playerName;
+    private boolean soundEnabled;
+    private int gameDifficulty;
+
+    public UserInterface(int boardSize, String playerName, boolean soundEnabled, int gameDifficulty) {
+        this.boardSize = boardSize;
+        this.playerName = playerName;
+        this.soundEnabled = soundEnabled;
+        this.gameDifficulty = gameDifficulty;
+    }
+
+    public int getBoardSize() {
+        return boardSize;
+    }
+
+    public void setBoardSize(int boardSize) {
+        this.boardSize = boardSize;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public boolean isSoundEnabled() {
+        return soundEnabled;
+    }
+
+    public void setSoundEnabled(boolean soundEnabled) {
+        this.soundEnabled = soundEnabled;
+    }
+
+    public int getGameDifficulty() {
+        return gameDifficulty;
+    }
+
+    public void setGameDifficulty(int gameDifficulty) {
+        this.gameDifficulty = gameDifficulty;
+    }
+
+    public void setOptions(UserInterface newOptions) {
+        this.boardSize = newOptions.getBoardSize();
+        this.playerName = newOptions.getPlayerName();
+        this.soundEnabled = newOptions.isSoundEnabled();
+        this.gameDifficulty = newOptions.getGameDifficulty();
+    }
+}
