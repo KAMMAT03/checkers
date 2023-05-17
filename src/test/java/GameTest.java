a118b07dd3b2b60d41c5a010ea5d29f147469bbd
Maciej Matuszewski <maciejmatuszewski030303@gmail.com>
2023-05-17 23:12:34 +0200

    dodanie testów jednostkowych

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,34 +1,40 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
 
 public class GameTest {
+
     private Game game;
 
     @BeforeEach
-    public void setUp() {
-        String input = "B"; // Wybór planszy klasycznej (10x10)
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-
-//        game = new Game();
+    public void setup() {
+        game = new Game(8); // Przykładowa wielkość planszy - można dostosować do potrzeb
     }
 
     @Test
-    public void testSetN() {
-        int expected = 10;
-        int actual = game.getN();
-        assertEquals(expected, actual);
+    public void testInitialGameSetup() {
+        Assertions.assertTrue(game.getPlayerTurn()); // Sprawdź, czy tura należy do białych graczy
+        Assertions.assertFalse(game.getGameOver()); // Sprawdź, czy gra nie jest zakończona
+        Assertions.assertFalse(game.getWinner()); // Sprawdź, czy nie ma jeszcze zwycięzcy
+        Assertions.assertEquals(8, game.getN()); // Sprawdź, czy wielkość planszy jest ustawiona na 8
+        // Dodaj inne asercje w zależności od oczekiwanego stanu gry na początku
     }
 
     @Test
-    public void testIsPlayerTurn() {
-        boolean expected = true;
-        boolean actual = game.isPlayerTurn();
-        assertEquals(expected, actual);
+    public void testMove() {
+        // Przygotowanie planszy i pionków dla testu
+        Board board = game.getBoard();
+        Field startField = board.getField(2, 0); // Wprowadź odpowiednie indeksy pola startowego
+        Field endField = board.getField(3, 1); // Wprowadź odpowiednie indeksy pola docelowego
+        List<Integer> wantedMoves = List.of(103); // Przykładowe żądane ruchy, które powinny być dostępne dla danego pionka
+
+
+        boolean moveResult = game.move(startField, endField, wantedMoves, board);
+
+        Assertions.assertFalse(moveResult); //sprawdzenie czy to błeędny ruch
     }
+
+
 }
