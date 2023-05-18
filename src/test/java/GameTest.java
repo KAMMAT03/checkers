0b90f1b996e319e46b73bfb1e6ed08a19a7e0492
Maciej Matuszewski <maciejmatuszewski030303@gmail.com>
2023-05-18 10:02:57 +0200

    zmiany w GameTest

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -10,24 +10,23 @@ public class GameTest {
 
     @BeforeEach
     public void setup() {
-        game = new Game(8); // Przykładowa wielkość planszy - można dostosować do potrzeb
+        game = new Game(8); // Przykładowa wielkość planszy
     }
 
     @Test
     public void testInitialGameSetup() {
-        Assertions.assertTrue(game.getPlayerTurn()); // Sprawdź, czy tura należy do białych graczy
-        Assertions.assertFalse(game.getGameOver()); // Sprawdź, czy gra nie jest zakończona
-        Assertions.assertFalse(game.getWinner()); // Sprawdź, czy nie ma jeszcze zwycięzcy
-        Assertions.assertEquals(8, game.getN()); // Sprawdź, czy wielkość planszy jest ustawiona na 8
-        // Dodaj inne asercje w zależności od oczekiwanego stanu gry na początku
+        Assertions.assertTrue(game.getPlayerTurn()); // sprawdzenie, czy tura należy do białych graczy
+        Assertions.assertFalse(game.getGameOver()); // sprawdzenie, czy gra nie jest zakończona
+        Assertions.assertFalse(game.getWinner()); // sprawdzenie, czy nie ma jeszcze zwycięzcy
+        Assertions.assertEquals(8, game.getN()); // sprawdzenie, czy wielkość planszy jest ustawiona na 8
     }
 
     @Test
     public void testMove() {
         // Przygotowanie planszy i pionków dla testu
         Board board = game.getBoard();
-        Field startField = board.getField(2, 0); // Wprowadź odpowiednie indeksy pola startowego
-        Field endField = board.getField(3, 1); // Wprowadź odpowiednie indeksy pola docelowego
+        Field startField = board.getFieldByIndex(2, 0); // wprowadzenie odpowiednich indeksów pola startowego
+        Field endField = board.getFieldByIndex(3, 1); // wprowadzenie odpowiednich indeksów pola docelowego
         List<Integer> wantedMoves = List.of(103); // Przykładowe żądane ruchy, które powinny być dostępne dla danego pionka
 
 
